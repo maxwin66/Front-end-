fb10ea257a40eb6a2191e7d4e0d99c48993d424b
Minatoz997 <Minatoz1997@gmail.com>
2025-06-02 23:53:18 +0700

    Update HomeSelect.tsx

--- components/HomeSelect.tsx
+++ components/HomeSelect.tsx
@@ -3,14 +3,13 @@ import React from "react";
 interface Props {
   onGoogle: () => void;
   onGuest: () => void;
+  bgStyle?: React.CSSProperties;
 }
 
-const HomeSelect: React.FC<Props> = ({ onGoogle, onGuest }) => (
+const HomeSelect: React.FC<Props> = ({ onGoogle, onGuest, bgStyle }) => (
   <div
     className="min-h-screen flex items-center justify-center"
-    style={{
-      background: "url('https://user-images.githubusercontent.com/107878113/321337217-6f05d6a6-9e94-4188-8f6f-2c0ef8b8d7b2.jpg') center/cover no-repeat"
-    }}
+    style={bgStyle}
   >
     <div className="bg-white/90 p-8 rounded-2xl max-w-sm w-full shadow-xl text-center">
       <h1 className="text-2xl font-bold text-blue-500 mb-4">Selamat Datang di MyKugy!</h1>

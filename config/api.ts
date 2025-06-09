58010d2a349b13c80462c8543d3a54413a022a37
Minatoz997 <Minatoz1997@gmail.com>
2025-06-10 04:58:14 +0700

    Update api.ts

--- config/api.ts
+++ config/api.ts
@@ -1,4 +1,4 @@
 export const VIRTUSIM_API = {
-  BASE_URL: 'https://virtusim.com/api/v2/json',
+  BASE_URL: 'https://virtusim.com/api/v2/json.php',
   KEY: process.env.NEXT_PUBLIC_VIRTUSIM_API_KEY || ''
 };
